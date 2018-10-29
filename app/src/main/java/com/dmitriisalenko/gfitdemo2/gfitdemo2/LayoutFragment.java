5fa56ca3a5434ffe794b4a260f65d9a0724ec8c2
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2018-10-29 14:47:33 +0200

    Clean up

--- app/src/main/java/com/dmitriisalenko/gfitdemo2/gfitdemo2/LayoutFragment.java
+++ app/src/main/java/com/dmitriisalenko/gfitdemo2/gfitdemo2/LayoutFragment.java
@@ -1,7 +1,6 @@
 package com.dmitriisalenko.gfitdemo2.gfitdemo2;
 
 import android.app.Fragment;
-import android.view.View;
 
 public abstract class LayoutFragment extends Fragment {
     public GoogleFitManager getGoogleFitManager() {
